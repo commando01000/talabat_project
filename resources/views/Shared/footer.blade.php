9c2c63a9cf4a67e8583f136d969067cd81d3aa81
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-15 00:45:32 +0300

    Footer_Completed !

--- resources/views/Shared/footer.blade.php
+++ resources/views/Shared/footer.blade.php
@@ -1,1 +1,133 @@
-<p>footer works !</p>
+<footer class="overflow-hidden">
+    <main class="container-fluid">
+        <div class="section footer-links d-flex w-50 justify-content-around pt-4 pb-2 m-auto"><a rel="noreferrer"
+                href="https://careers.deliveryhero.com/talabat" class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-1"
+                target="_blank">Careers</a>
+            <div class="breaker"></div><a rel="noreferrer" href="/uae/terms"
+                class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-3" target="_self">Terms and Conditions</a><a
+                rel="noreferrer" href="/uae/faq" class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-3"
+                target="_self">FAQ</a><a rel="noreferrer" href="/uae/privacy"
+                class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-3 text-capitalize" target="_self">Privacy
+                Policy</a><a rel="noreferrer" href="/uae/contact-us"
+                class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-3" target="_self">Contact Us</a>
+            <div class="breaker"></div><a rel="noreferrer" href="/uae/sitemap"
+                class="sc-e8dc6632-1 bLmHjr f-m-14 pt-m-4 pl-m-5 mt-m-3" target="_self">Sitemap</a>
+        </div>
+        <hr style="color: #999">
+        <div class="pupular-links pt-2">
+            <div class="container-fluid">
+                <div class="row gx-5 pb-3 w-50 m-auto justify-content-end">
+                    <div class="col-sm-2 text-start">
+                        <p class="">Restaurants</p>
+                        <div class="row flex-column">
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Burger King</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Texas Chicken</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">PizzaExpress</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Johnny Rockets</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">More Restaurants</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2 text-start">
+                        <p>Pupular Cuisines</p>
+                        <div class="row flex-column">
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">American</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Breakfast</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Burgers</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Cafe</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">Chinese</a>
+                            </div>
+                            <div class="col-sm-12">
+                                <a class="text-white" href="#">More Cuisines</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2 text-start">
+                        <p>Popular Areas</p>
+                        <div class="row flex-column">
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Abu Hail</a>
+
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Academic City</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Al Awir</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Al Bada'a</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Al baraha</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">More Areas</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2 text-start">
+                        <p>Cities</p>
+                        <div class="row flex-column">
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Dubai</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Abu Dhabi</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Sharjah</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Ajman</a>
+                            </div>
+                            <div class="col-md-12">
+                                <a class="text-white" href="#">Al Ain</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 text-start">
+                        <p>Follow us on</p>
+                        <div class="row flex-row">
+                            <div class="col-md-12">
+                                {{-- facebook icon --}}
+                                <a class="text-white px-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a class="text-white px-2" href="#"><i class="fab fa-twitter"></i></a>
+                                <a class="text-white px-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                                <a class="text-white px-2" href="#"><i class="fab fa-instagram"></i></a>
+                                <a class="text-white px-2" href="#"><i class="fab fa-youtube"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr style="color: #999">
+                <div class="row w-50 m-auto d-flex flex-nowrap justify-content-between" style="color: #999">
+                    <div class="col">
+                        <p>For any support or help you can contact us via our Live Chat</p>
+                    </div>
+                    <div class="col text-end">
+                        <p>©2024 Talabat.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</footer>
