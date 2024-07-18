20fbb129ac4b35ceec2bbf7a6d0510f2a3450a1b
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-18 09:15:26 +0300

    embedding a map

--- resources/views/Frontend/home/index.blade.php
+++ resources/views/Frontend/home/index.blade.php
@@ -442,6 +442,13 @@
         </div>
     </div>
 
+    <div class="w-100 pb-5 text-center">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d231105.66102029695!2d55.193098792815356!3d25.168601496313677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2sDubai%20-%20United%20Arab%20Emirates!5e0!3m2!1sen!2seg!4v1721283021296!5m2!1sen!2seg"
+            width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+
     <div class="discover-our-new-tablet-app">
         <div class="container">
             <div class="row">
